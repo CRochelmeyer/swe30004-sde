c2e743fa66af73e71ca44491e30b3915280bb8ea
Claire Rochelmeyer <7671148@student.swin.edu.au>
2017-10-24 00:25:25 +1100

    ~ changed php files

--- welcome.php
+++ welcome.php
@@ -4,9 +4,43 @@
 </head>
 <body>
     <?php
+	
+	$firstname = $_POST["firstname"];
+	$lastname = $_POST["lastname"];
+	$tyre_no = $_POST["tyres"];
 
-    echo $_POST["firstname"]." ".$_POST["lastname"].'<br/>';
-    echo "total amount due is: ".($_POST["tyres"]* 110).'<br/>';
+    echo $firstname." ".$lastname'<br/>';
+    echo "total amount due is: ".($tyre_no* 110).'<br/>';
+	
+    $servername = "localhost";
+    $user="abc";
+    $password="abc";
+    $dbname="Assignmentdb";
+
+    // Create connection
+    $conn = new mysqli($servername,$user,$password,$dbname);
+
+    // Check connection
+    if ($conn->connect_error)
+	{
+		die("Connection failed: " . $conn->connect_error);
+    }
+	else
+	{
+		$sql = "INSERT INTO Orders (firstname, lastname, noOftyres)
+		VALUES ('$firstname', '$lastname', '$tyre_no)";
+
+		if ($conn->query($sql) === TRUE)
+		{
+			echo "New record created successfully";
+		}
+		else
+		{
+			echo "Error: " . $sql . "<br>" . $conn->error;
+		}
+
+		$conn->close();
+	}
 
     ?>
 </body>
