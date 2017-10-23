0017dd793152d92fbc16fa5de977fd76f1992147
Claire Rochelmeyer <7671148@student.swin.edu.au>
2017-10-23 23:28:03 +1100

    ~ added if not exists

--- createtable.sql
+++ createtable.sql
@@ -1,8 +1,8 @@
-create database Assignmentdb;
+create database IF NOT EXISTS Assignmentdb;
 
 go
 
-Create table Orders(
+Create table IF NOT EXISTS Orders(
 firstname varchar(50) not null primary key,
 lastname varchar(50) not null,
 noOftyres int not null);
