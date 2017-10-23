6ecbd48cf71d8c80c60b5123bfd0d8ce2ac82471
Claire Rochelmeyer <7671148@student.swin.edu.au>
2017-10-23 21:25:53 +1100

    ~ tweaked create table to not override old one if it exists already

--- createtable.sql
+++ createtable.sql
@@ -2,7 +2,7 @@ create database Assignmentdb;
 
 go
 
-Create table Orders(
+Create table IF NOT EXISTS Orders(
 firstname varchar(50) not null primary key,
 lastname varchar(50) not null,
 noOftyres int not null);
