bbbecd026d06dee30b4921a9b723dc9f63fbc4bd
Claire Rochelmeyer <7671148@student.swin.edu.au>
2017-10-23 23:08:12 +1100

    ~ revert create table change

--- createtable.sql
+++ createtable.sql
@@ -2,7 +2,7 @@ create database Assignmentdb;
 
 go
 
-Create table IF NOT EXISTS Orders(
+Create table Orders(
 firstname varchar(50) not null primary key,
 lastname varchar(50) not null,
 noOftyres int not null);
